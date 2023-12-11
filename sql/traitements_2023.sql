f358de5c633f502068d6bffbebce06781cacbf7e
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2023-12-11 17:38:58 +0100

    Adding command \rev

--- sql/traitements_2023.sql
+++ sql/traitements_2023.sql
@@ -11,4 +11,15 @@ AND ope_date_debut>'2023-08-01'
 
 UPDATE t_lot_lot SET lot_effectif = 0 WHERE lot_identifiant = 440274;--1
 
+-- Lamproies fluviatiles
+
+
+WITH troppetite AS (
+SELECT lot_identifiant FROM iav.vue_lot_ope_car_qan  
+WHERE ope_dic_identifiant =5 AND lot_tax_code='2014'
+AND car_valeur_quantitatif <500)
+UPDATE iav.t_lot_lot SET lot_tax_code =2011
+WHERE lot_identifiant IN (
+SELECT lot_identifiant FROM troppetite); --7
+
 
